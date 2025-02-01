76d51f2925a41d976f8a17b0f295d5c5942975aa
Vaniawl <idovhosheia@gmail.com>
2025-02-01 09:23:19 +0100

    Initial Commit

---
+++ dictionatyNew/dictionatyNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  dictionatyNewApp.swift
+//  dictionatyNew
+//
+//  Created by Ivan Dovhosheia on 01.02.25.
+//
+
+import SwiftUI
+
+@main
+struct dictionatyNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
